4f599d860c0e8be2f924cc2fb7ffe8f8bcc3852b
AkShAy21211 <akshay68580@gmail.com>
2024-10-01 10:10:32 +0530

    url change

--- app/components/TopicList.jsx
+++ app/components/TopicList.jsx
@@ -1,33 +1,40 @@
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 import RemoveButton from "./RemoveButton";
 import Link from "next/link";
 import { HiPencilAlt } from "react-icons/hi";
 
-console.log(process.env.NEXT_PUBLIC_API_URL);
-
-export const getTopics = async () => {
-  try {
-    const response = await fetch(process.env.NEXT_PUBLIC_API_URL+"/api/topics", {
-      cache: "no-store",
-    });
-    if (!response.ok) {
-      throw new Error("Failed to fetch topics");
-    }
-
-    
-    return response.json();
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 const TopicList = async () => {
+
+
+
+
+  const getTopics = async () => {
+    try {
+      const response = await fetch(
+        process.env.NEXT_PUBLIC_API_URL + "/api/topics",
+        {
+          cache: "no-store",
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch topics");
+      }
+
+      return response.json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
   const topics = await getTopics();
 
   return (
     <>
       {topics.map((t, i) => (
-        <div key={t._id} className="p-4 flex  justify-between  border-slate-300 gap-5 my-3 items-center">
+        <div
+          key={t._id}
+          className="p-4 flex  justify-between  border-slate-300 gap-5 my-3 items-center"
+        >
           <div>
             <h2 className="font-bold text-2xl">{t.title}</h2>
 
